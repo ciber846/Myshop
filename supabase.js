0daeb8d3c471ae184506db41c9ad654e97c1bb2f
shahinur-C6b3r <151050566+ciber846@users.noreply.github.com>
2026-08-30 10:16:40 +0600

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+  "https://vylesixmipqtzywnotia.supabase.co";
+
+const SUPABASE_KEY =
+  "YOUR_NEW_PUBLISHABLE_KEY";
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
